fa66d3a6b0b5e27e016fe16c453c3b27f143bccb
lordsam007 <samdevelops7@gmail.com>
2023-09-17 18:24:30 +0530

    GEO LOCATOR

---
+++ myscript.js
@@ -0,0 +1,20 @@
+﻿const elLatitude = document.getElementById("id_Latitude");
+const elLongitude = document.getElementById("id_Longitude");
+const elGogleMap = document.getElementById("id_GoogleMap");
+
+navigator.geolocation.getCurrentPosition(success);
+
+let latitude;
+let longitude;
+
+function success(position) {
+  latitude = position.coords.latitude;
+  longitude = position.coords.longitude;
+  elLatitude.innerText = "Latitude=" + latitude;
+  elLongitude.innerText = "Longitude=" + longitude;
+}
+
+elGogleMap.onclick = () => {
+  const url = "https://www.google.com/maps?q=" + latitude + "," + longitude;
+  chrome.tabs.create({ url: url });
+}
